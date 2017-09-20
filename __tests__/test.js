50e0707b96fa65ba584b31b9db5cb6dc3cf10a7b
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-09-20 17:19:48 +0200

    Added some temaplates and basic test file configuration

--- __tests__/test.js
+++ __tests__/test.js
@@ -3,3 +3,10 @@ const helpers = require('yeoman-test');
 const path = require('path');
 
 let tempDir = '';
+
+
+describe('Basic scaffolding generator', () => {
+  it('creates all eslint files', () => {
+
+  });
+});
