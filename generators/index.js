d55ffb656b629423ce13ca2513ab911d082af0f8
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-09-20 14:03:13 +0200

    Configuring generator

---
+++ generators/index.js
@@ -0,0 +1,21 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class SimpleScaffolder extends Generator {
+  // Instantiate Yeoman Generator
+  constructor(args, opts) {
+      super(args, opts);
+  }
+
+  // Ask the user for his preferences
+  prompting() {
+    return this.prompt([{
+      type: "input",
+      name: "name",
+      message: "Dummy message",
+      default: this.appname // Default to the current folder,
+      store: true
+    }]).then( (answers) => {
+      this.log('appname', answers.name)
+    })
+  }
+};
